1ce205b3cbc058b0ce1f0befd1d3f15ff3d435a1
Shriharsh07 <shriharshdpattar@gmail.com>
2025-06-29 15:09:16 +0530

    added dev backend url

--- src/app/services/apis.service.ts
+++ src/app/services/apis.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApisService {
- baseUrl: string = 'http://localhost:8080'
+ baseUrl: string = 'https://inventory-management-backend-a244.onrender.com'
 
   constructor(private _http: HttpClient) { }
 
